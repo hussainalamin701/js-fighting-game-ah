dcd1ab4e88edecc72b9b8d8cbbc67c66f78a2dd3
Alamin Hussain <140621473+hussainalamin701@users.noreply.github.com>
2025-06-25 12:19:52 +0100

    Fully implemented player 1 movement and jumping however having issues with correcting flip functionality, progressing to working on replicating for the player 2 movement

--- main.js
+++ main.js
@@ -158,23 +158,28 @@ scene("fight", () => {
    player1.use(sprite(player1.sprites.idle))
    player1.play("idle")
 
-   function run(player, speed, flipP){
+   function run(player, speed, flipPlayer){
     if(player1.health === 0){
         return
     }
-
+    if(player.curAnim() !== "run"
+        && !player.isCurrentlyJumping){
+        player.use(sprite(player.sprites.run))
+        player.play("run")
+    }
         player.move(speed, 0)
-        player.flip = flipP
-   }
-   function resetPlayerToIdle(player) {
-        player.use(sprite(player.sprites.idle))
-        player.play("idle")
+        player.flip = flipPlayer
    }
 
-   onKeyDown("d", () => {
+   function resetPlayerToIdle(player) {
+    player.use(sprite(player.sprites.idle))
+    player.play("idle")
+    }
+
+    onKeyDown("d", () => {
         run(player1, 500, false)
-   })
-   onKeyRelease("d", () => {
+    })
+    onKeyRelease("d", () => {
         if (player1.health !== 0) {
             resetPlayerToIdle(player1)
             player1.flipX = false
@@ -182,15 +187,81 @@ scene("fight", () => {
     })
 
     onKeyDown("a", () => {
-        run(player1, -500, false)
-   })
-   onKeyRelease("a", () => {
+        run(player1, -500, true)
+    })
+    onKeyRelease("a", () => {
         if (player1.health !== 0) {
             resetPlayerToIdle(player1)
-            player1.flipX = false
+            player1.flipX = true
         }
     })
 
+    function makeJump(player){
+        if(player.health === 0){
+            return
+        }
+        if(player.isGrounded()){
+            const currentFlip = player.flipX
+            player.jump()
+            player.use(sprite(player.sprites.jump))
+            player.flipX = currentFlip
+            player.play("jump")
+            player.isCurrentlyJumping = true
+        }
+    }
+
+    function resetAfterJump(player){
+        if(player.isGrounded() && player.isCurrentlyJumping){
+            player.isCurrentlyJumping = false
+            if(player.curAnim() !== "idle"){
+                resetPlayerToIdle(player)
+            }
+        }
+    }
+    onKeyDown("w", () => {
+        makeJump(player1)
+    })
+
+    player1.onUpdate(() => resetAfterJump(player1))
+
+    function attack(player, excludedKeys){
+        if(player.health === 0){
+            return
+        }
+
+        for(const key of excludedKeys){
+            if(isKeyDown(key)){
+                return
+            }
+        }
+
+        const currentFlip = player.flipX
+        if(player.curAnim() !== "attack"){
+            player.use(sprite(player.sprites.attack))
+            player.flipX = currentFlip
+            const slashX = player.pos.x + 30
+            const slashXFlipped = player.pos.x - 350
+            const slashY = player.pos.y - 200
+
+            add([
+                rect(300, 300),
+                area(),
+                pos(currentFlip ? slashXFlipped: slashX, slashY),
+                opacity(0),
+                player.id + "attackHitbox"
+            ])
+
+            player.play("attack", {
+                onEnd: () => {
+                    resetPlayerToIdle(player)
+                    player.flipX = currentFlip
+                }
+            })
+        }
+    }
+    onKeyDown("space", () => {
+        attack(player1, ["a", "d", "w"])
+    })
 })
 
 go("fight")
